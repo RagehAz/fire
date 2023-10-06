1ec1676547a42b52be73219bec1923de50eb6339
RagehAz <rageh.az@gmail.com>
2023-10-06 03:29:14 +0200

    2.2.9 : real doc streaming in both official and native are good

--- lib/e_streamers/real_doc_streamer.dart
+++ lib/e_streamers/real_doc_streamer.dart
@@ -6,58 +6,55 @@ class RealDocStreamer extends StatelessWidget {
     required this.coll,
     required this.doc,
     required this.builder,
-    this.loadingWidget,
-    this.noValueWidget,
     super.key
   });
   /// --------------------------------------------------------------------------
   final String coll;
   final String doc;
-  final Widget? loadingWidget;
-  final Widget? noValueWidget;
-  final Widget Function(BuildContext, Map<String, dynamic>) builder;
+  final Widget Function(bool loading, Map<String, dynamic>? map) builder;
   /// --------------------------------------------------------------------------
   @override
   Widget build(BuildContext context) {
 
-    return StreamBuilder<f_db.DatabaseEvent>(
-        stream: f_db.FirebaseDatabase.instance.ref('$coll/$doc').onValue,
+    if (FirebaseInitializer.isUsingOfficialPackages() == true){
+
+      return StreamBuilder<f_db.DatabaseEvent>(
+        stream: OfficialFirebase.getReal()?.ref('$coll/$doc').onValue,
         builder: (_, snapshot){
 
-          /// LOADING
-          if (snapshot.connectionState == ConnectionState.waiting){
-            return loadingWidget ?? const SizedBox();
-          }
+          final f_db.DatabaseEvent? _event = snapshot.data;
+          final f_db.DataSnapshot? _snap = _event?.snapshot;
+          final Map<String, dynamic>? _map = _OfficialFireMapper.getMapFromDataSnapshot(
+            snapshot: _snap,
+            addDocID: true,
+          );
 
-          /// NO DATA
-          else if (snapshot.hasData == false){
-            return noValueWidget ?? const SizedBox();
-          }
-
-          /// RECEIVED DATA
-          else {
-
-            final f_db.DatabaseEvent? _event = snapshot.data;
-            final f_db.DataSnapshot? _snap = _event?.snapshot;
-            final Map<String, dynamic>? _map = _OfficialFireMapper.getMapFromDataSnapshot(
-              snapshot: _snap,
-              addDocID: true,
-            );
-
-            /// NO DATA
-            if (_map == null){
-              return noValueWidget ?? const SizedBox();
-            }
-
-            /// DATA IS GOOD
-            else {
-              return builder(context, _map);
-            }
-
-          }
+          return builder(snapshot.connectionState == ConnectionState.waiting, _map);
 
         }
     );
+
+    }
+
+    else {
+
+      return StreamBuilder<f_d.Event>(
+        stream: _NativeFirebase.getReal()?.reference().child(coll).child(doc).onValue,
+        builder: (_, snapshot){
+
+          final f_d.Event? _event = snapshot.data;
+          final f_d.DataSnapshot? _snap = _event?.snapshot;
+          final Map<String, dynamic>? _map = _NativeFireMapper.getMapFromDataSnapshot(
+            snapshot: _snap,
+            addDocID: true,
+          );
+
+          return builder(snapshot.connectionState == ConnectionState.waiting, _map);
+
+        }
+    );
+
+    }
 
   }
 /// --------------------------------------------------------------------------
