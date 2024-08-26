8a9545378bbbfa6442a3c540f506133afa144537
RagehAz <rageh.az@gmail.com>
2024-08-27 01:55:45 +0300

    4.6: Lets publish 4.6

--- lib/f_paginators/official_fire_coll_paginator.dart
+++ lib/f_paginators/official_fire_coll_paginator.dart
@@ -242,7 +242,7 @@ class _OfficialFireCollPaginatorState extends State<OfficialFireCollPaginator> {
   Future<void> _readMoreOfficial() async {
     
     /// CAN KEEP READING
-    if (_paginatorController.canKeepReading.value  == true){
+    if (mounted == true && _paginatorController.canKeepReading.value  == true){
 
       final List<Map<String, dynamic>>? _nextMaps = await OfficialFire.readColl(
         queryModel: widget.paginationQuery,
